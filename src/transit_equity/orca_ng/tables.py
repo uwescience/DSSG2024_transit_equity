cc6e181b9e981e96c0bb1e268ed099c39980ee72
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-06 19:29:20 -0700

    Remove dynamic enum or dict creation since autocompletion does not work despite typing assignment

--- src/transit_equity/orca_ng/tables.py
+++ src/transit_equity/orca_ng/tables.py
@@ -3,31 +3,19 @@ This file contains all the table, view and materialized view names in the databa
 The names are hard-coded because the database schema is static.
 """
 
-from .schemas import DSSG_SCHEMA, ORCA_SCHEMA, TRAC_SCHEMA, GTFS_SCHEMA
+from enum import Enum
 
-def add_schema_to_tables(table_dict: dict, schema_name: str):
-    return {table_key: schema_name+'.'+table_name for table_key, table_name in table_dict.items()}
+class DSSG_TABLES(Enum):
+    BOARDINGS_VIEW: str = 'v_boardings_apr2023'
+    LINKED_TRANSACTIONS_VIEW: str = 'v_linked_transactions_apr2023'
+    TRANSACTIONS_MAT_VIEW: str = 'm_transactions_apr2023'
 
-DSSG_TABLES = {
-    'BOARDINGS_VIEW': 'v_boardings_apr2023',
-    'LINKED_TRANSACTIONS_VIEW': 'v_linked_transactions_apr2023',
+class ORCA_TABLES(Enum):
+    TRANSACTION_TYPES_TABLE: str = 'transaction_types',
+    MODES_TABLE: str = 'modes'
 
-    'TRANSACTIONS_MAT_VIEW': 'm_transactions_apr2023'
-}
-DSSG_SCHEMA_TABLES = add_schema_to_tables(DSSG_TABLES, DSSG_SCHEMA)
+class TRAC_TABLES(Enum):
+    AGENCIES_TABLE: str = 'agencies'
 
-ORCA_TABLES = {
-    'TRANSACTION_TYPES_TABLE': 'transaction_types',
-    'MODES_TABLE': 'modes'
-}
-ORCA_SCHEMA_TABLES = add_schema_to_tables(ORCA_TABLES, ORCA_SCHEMA)
-
-TRAC_TABLES = {
-    'AGENCIES_TABLE': 'agencies'
-}
-TRAC_SCHEMA_TABLES = add_schema_to_tables(TRAC_TABLES, TRAC_SCHEMA)
-
-GTFS_TABLES = {
-    'FEED_INFO_TABLE': 'tl_feed_info'
-}
-GTFS_SCHEMA_TABLES = add_schema_to_tables(GTFS_TABLES, GTFS_SCHEMA)
+class GTFS_TABLES(Enum):
+    FEED_INFO_TABLE: str = 'tl_feed_info'
