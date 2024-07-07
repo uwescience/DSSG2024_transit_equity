5bdcb5b6e84a176c164346d7e6648e89bcd7afee
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-06 19:31:02 -0700

    Naming convention change in schema tables

--- src/transit_equity/orca_ng/schema_tables.py
+++ src/transit_equity/orca_ng/schema_tables.py
@@ -8,17 +8,17 @@ from enum import Enum
 
 from .schemas import DSSG_SCHEMA, ORCA_SCHEMA, TRAC_SCHEMA, GTFS_SCHEMA
 
-class DSSG_TABLES(Enum):
+class DSSG_SCHEMA_TABLES(Enum):
     BOARDINGS_VIEW: str = f'{DSSG_SCHEMA}.v_boardings_apr2023'
     LINKED_TRANSACTIONS_VIEW: str = f'{DSSG_SCHEMA}.v_linked_transactions_apr2023'
     TRANSACTIONS_MAT_VIEW: str = f'{DSSG_SCHEMA}.m_transactions_apr2023'
 
-class ORCA_TABLES(Enum):
+class ORCA_SCHEMA_TABLES(Enum):
     TRANSACTION_TYPES_TABLE: str = f'{ORCA_SCHEMA}.transaction_types',
     MODES_TABLE: str = f'{ORCA_SCHEMA}.modes'
 
-class TRAC_TABLES(Enum):
+class TRAC_SCHEMA_TABLES(Enum):
     AGENCIES_TABLE: str = f'{TRAC_SCHEMA}.agencies'
 
-class GTFS_TABLES(Enum):
+class GTFS_SCHEMA_TABLES(Enum):
     FEED_INFO_TABLE: str = f'{GTFS_SCHEMA}.tl_feed_info'
