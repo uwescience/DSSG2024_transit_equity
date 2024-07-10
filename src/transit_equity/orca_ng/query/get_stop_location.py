d89533f3a1d228621f43e049e733d4ab302e1bd3
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-10 15:55:43 -0700

    Complete query logic for stop location

--- src/transit_equity/orca_ng/query/get_stop_location.py
+++ src/transit_equity/orca_ng/query/get_stop_location.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Table, Select
+from sqlalchemy import Table, Select, func, select, not_, or_, and_, case
 from sqlalchemy.ext.automap import AutomapBase
 
 from . import get_schema_key
@@ -12,7 +12,7 @@ def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, e
     '''
     This function returns a query that can be used to get transactions with their stop locations.
     The transactions table is joined with the gtfs stop locations data.
-    For each stop, we get the latest GTFS feed and assign the stop location from that feed.
+    For each stop, we get the latest GTFS feed for each transit agency and assign the stop location from that feed.
 
     Parameters
     ----------
@@ -59,5 +59,68 @@ def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, e
     if not transactions_t:
         transactions_t = Base_orca.metadata.tables[ORCA_SCHEMA_TABLES.TRANSACTIONS_TABLE]
     
-    boardings_v = Base_dssg.metadata.tables[DSSG_SCHEMA_TABLES.BOARDINGS_VIEW.value]
+    agencies = Base_trac.metadata.tables[TRAC_SCHEMA_TABLES.AGENCIES_TABLE.value]
+    modes = Base_orca.metadata.tables[ORCA_SCHEMA_TABLES.MODES_TABLE.value]
+    feed_info = Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.FEED_INFO_TABLE.value]
+    stops = Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.STOPS_TABLE.value]
+    agencies_gtfs = Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.AGENCY_TABLE.value]
 
+    # Get the feeds only for the given date range
+    stmt_gtfs_feed_april_2023 = \
+        select(feed_info)\
+        .where(not_(or_(feed_info.c.feed_start_date >= start_date, 
+                        feed_info.c.feed_end_date <= end_date)))
+    # print(stmt_gtfs_feed_april_2023)
+
+    stmt_gtfs_feed_april_2023_alias = stmt_gtfs_feed_april_2023.subquery('feed_april')
+
+    # Rank the feeds for the given date range
+    stmt_gtfs_feed_april_2023_ranked = \
+    select(stmt_gtfs_feed_april_2023_alias, func.row_number().over(
+        partition_by=stmt_gtfs_feed_april_2023_alias.c.feed_publisher_name,
+        order_by=stmt_gtfs_feed_april_2023_alias.c.feed_id.desc()).label('feed_rank'))
+    # print(stmt_gtfs_feed_april_2023_ranked)
+
+    stmt_gtfs_feed_april_2023_ranked_alias = stmt_gtfs_feed_april_2023_ranked.subquery('feed_april_ranked')
+
+    # Finally, get latest feed within the April 2023 range, for each transit agency
+    stmt_gtfs_feed_april_2023_latest = \
+        select(stmt_gtfs_feed_april_2023_ranked_alias)\
+        .where(stmt_gtfs_feed_april_2023_ranked_alias.c.feed_rank == 1)
+    # print(stmt_gtfs_feed_april_2023_latest)
+
+    stmt_gtfs_feed_april_2023_latest_alias = stmt_gtfs_feed_april_2023_latest.cte('feed_april_latest')
+
+    stmt_stop_april_2023_latest = \
+        select(stops, agencies_gtfs.c.agency_id, agencies_gtfs.c.agency_name)\
+        .join(stmt_gtfs_feed_april_2023_latest_alias, stops.c.feed_id == stmt_gtfs_feed_april_2023_latest_alias.c.feed_id)\
+        .join(agencies_gtfs, stmt_gtfs_feed_april_2023_latest_alias.c.feed_id == agencies_gtfs.c.feed_id)
+    # print(stmt_stop_april_2023_latest)
+
+    stmt_boardings_april_2023_with_agency = \
+        select(transactions_t, agencies.c.agency_id, agencies.c.orca_agency_id, agencies.c.gtfs_agency_id, agencies.c.agency_name)\
+        .join(agencies, transactions_t.c.source_agency_id == agencies.c.orca_agency_id)\
+        .where(not_(and_(transactions_t.c.stop_id.is_(None), transactions_t.c.device_location.is_(None))))
+    # print(stmt_boardings_april_2023_with_agency)
+
+    stmt_stop_april_2023_latest_alias = stmt_stop_april_2023_latest.cte('stop_april_latest')
+    stmt_boardings_april_2023_with_agency_alias = stmt_boardings_april_2023_with_agency.cte('boardings_april_with_agency')
+
+    stmt_boardings_april_2023_with_location = \
+    select(stmt_boardings_april_2023_with_agency_alias,
+        case((stmt_boardings_april_2023_with_agency_alias.c.device_location.is_not(None), stmt_boardings_april_2023_with_agency_alias.c.device_location),
+            else_=stmt_stop_april_2023_latest_alias.c.stop_location).label('boarding_location'))\
+        .join(stmt_stop_april_2023_latest_alias,
+            and_(stmt_boardings_april_2023_with_agency_alias.c.stop_code == stmt_stop_april_2023_latest_alias.c.stop_id,
+                stmt_boardings_april_2023_with_agency_alias.c.gtfs_agency_id == stmt_stop_april_2023_latest_alias.c.agency_id),
+                isouter=True)
+    # print(stmt_boardings_april_2023_with_location)
+
+    stmt_boardings_april_2023_with_location_alias = stmt_boardings_april_2023_with_location.subquery('boardings_april_with_location')
+
+    stmt_boardings_april_2023_with_location_alias_not_null = \
+        select(stmt_boardings_april_2023_with_location_alias)\
+        .where(stmt_boardings_april_2023_with_location_alias.c.boarding_location.is_(None))
+    # print(stmt_boardings_april_2023_with_location_alias_not_null)
+
+    return stmt_boardings_april_2023_with_location_alias_not_null
