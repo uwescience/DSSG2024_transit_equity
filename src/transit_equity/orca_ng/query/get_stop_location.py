063d1bf04408a15fb60a23ae5b2786c18ad76876
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-17 10:08:37 -0700

    Set up general class for getting stop locations

--- src/transit_equity/orca_ng/query/get_stop_location.py
+++ src/transit_equity/orca_ng/query/get_stop_location.py
@@ -1,11 +1,102 @@
 import datetime
 
-from sqlalchemy import Table, Select, func, select, not_, or_, and_, case
+from sqlalchemy import Engine
+from sqlalchemy import Table, Select
+from sqlalchemy import func, select, not_, or_, and_, case
 from sqlalchemy.ext.automap import AutomapBase
 
 from . import get_schema_key
 from ..constants.schemas import DSSG_SCHEMA, ORCA_SCHEMA, TRAC_SCHEMA, GTFS_SCHEMA
 from ..constants.schema_tables import DSSG_SCHEMA_TABLES, ORCA_SCHEMA_TABLES, TRAC_SCHEMA_TABLES, GTFS_SCHEMA_TABLES
+from ...utils.db_helpers import get_automap_base_with_views
+
+class StopLocationsFromTransactions:
+    '''
+    A class to get stop locations from transactions using customizable logic.
+
+    Attributes
+    ----------
+    start_date : datetime
+        Earliest possible start date for the transactions. It is imperative to add start_date for performance reasons.
+    end_date : datetime
+        Earliest possible end date for the transactions. It is imperative to add end_date for performance reasons.
+    engine : sqlalchemy.Engine
+        Engine that is already connected to a database
+    transactions_t : sqlalchemy.Table, optional
+        Table object for the transactions table. If not provided, the default orca.transactions table is used
+    '''
+    def __init__(self, start_date: datetime, end_date: datetime, engine: Engine, transactions_t: Table | None = None):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.engine = engine
+        self.transactions_t = transactions_t
+        self.get_automap_bases()
+
+    def get_automap_bases(self):
+        self.Base_dssg = get_automap_base_with_views(engine=self.engine, schema=DSSG_SCHEMA)
+        self.Base_trac = get_automap_base_with_views(engine=self.engine, schema=TRAC_SCHEMA)
+        self.Base_orca = get_automap_base_with_views(engine=self.engine, schema=ORCA_SCHEMA)
+        self.Base_gtfs = get_automap_base_with_views(engine=self.engine, schema=GTFS_SCHEMA)
+    
+    def get_latest_gtfs_feed(self):
+        '''
+        This function returns a query that can be used to get the latest GTFS feed for each transit agency.
+
+        Returns
+        -------
+        select : sqlalchemy.sql.selectable.Select
+            A select query that can be used to get the latest GTFS feed for each transit agency
+        '''
+        feed_info = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.FEED_INFO_TABLE.value]
+
+        # Get the feeds only for the given date range
+        stmt_gtfs_feed = \
+            select(feed_info)\
+            .where(not_(or_(feed_info.c.feed_start_date >= self.start_date, 
+                            feed_info.c.feed_end_date <= self.end_date)))
+
+        stmt_gtfs_feed_alias = stmt_gtfs_feed.subquery('feed')
+
+        # Rank the feeds for the given date range
+        stmt_gtfs_feed_ranked = \
+        select(stmt_gtfs_feed_alias, func.row_number().over(
+            partition_by=stmt_gtfs_feed_alias.c.feed_publisher_name,
+            order_by=stmt_gtfs_feed_alias.c.feed_id.desc()).label('feed_rank'))
+
+        stmt_gtfs_feed_ranked_alias = stmt_gtfs_feed_ranked.subquery('feed_ranked')
+
+        # Finally, get latest feed within the date range, for each transit agency, to get the feed_latest CTE
+        stmt_gtfs_feed_latest = \
+            select(stmt_gtfs_feed_ranked_alias)\
+            .where(stmt_gtfs_feed_ranked_alias.c.feed_rank == 1)
+
+        return stmt_gtfs_feed_latest
+    
+    def get_stop_details_from_feed(self, stmt_gtfs_feed: Select):
+        '''
+        This function returns a query that can be used to get the stop details based on the given feeds.
+
+        Parameters
+        ----------
+        stmt_gtfs_feed : sqlalchemy.sql.selectable.Select
+            A select query that can be used to get relevant GTFS feeds based on customizable logic
+
+        Returns
+        -------
+        select : sqlalchemy.sql.selectable.Select
+            A select query that can be used to get the stop details based on the given feeds
+        '''
+        stops = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.STOPS_TABLE.value]
+        agencies_gtfs = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.AGENCY_TABLE.value]
+
+        stmt_gtfs_feed_alias = stmt_gtfs_feed.cte('feed_custom')
+
+        stmt_stop = \
+            select(stops, agencies_gtfs.c.agency_id, agencies_gtfs.c.agency_name)\
+            .join(stmt_gtfs_feed_alias, stops.c.feed_id == stmt_gtfs_feed_alias.c.feed_id)\
+            .join(agencies_gtfs, stmt_gtfs_feed_alias.c.feed_id == agencies_gtfs.c.feed_id)
+
+        return stmt_stop
 
 def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, end_date: datetime, 
     automap_base_dict: dict, transactions_t: Table | None = None) -> Select:
@@ -49,9 +140,6 @@ def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, e
     >>> print(type(query))
     '''
     # Keys for schemas of interest
-    schemas_required = [DSSG_SCHEMA, ORCA_SCHEMA, TRAC_SCHEMA, GTFS_SCHEMA]
-    schema_keys = [get_schema_key(schema_name) for schema_name in schemas_required]
-    Base_dssg: AutomapBase = automap_base_dict[get_schema_key(DSSG_SCHEMA)]
     Base_trac: AutomapBase = automap_base_dict[get_schema_key(TRAC_SCHEMA)]
     Base_orca: AutomapBase = automap_base_dict[get_schema_key(ORCA_SCHEMA)]
     Base_gtfs: AutomapBase = automap_base_dict[get_schema_key(GTFS_SCHEMA)]
