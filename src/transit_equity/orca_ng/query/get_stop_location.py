545203f3efc008dadf38313e80b00d9caec4a691
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-17 10:28:11 -0700

    Remove class code from original file

--- src/transit_equity/orca_ng/query/get_stop_location.py
+++ src/transit_equity/orca_ng/query/get_stop_location.py
@@ -10,123 +10,6 @@ from ..constants.schemas import DSSG_SCHEMA, ORCA_SCHEMA, TRAC_SCHEMA, GTFS_SCHE
 from ..constants.schema_tables import DSSG_SCHEMA_TABLES, ORCA_SCHEMA_TABLES, TRAC_SCHEMA_TABLES, GTFS_SCHEMA_TABLES
 from ...utils.db_helpers import get_automap_base_with_views
 
-class TransactionsWithLocations:
-    '''
-    A class to get stop locations from transactions using customizable logic.
-
-    Attributes
-    ----------
-    start_date : datetime
-        Earliest possible start date for the transactions. It is imperative to add start_date for performance reasons.
-    end_date : datetime
-        Earliest possible end date for the transactions. It is imperative to add end_date for performance reasons.
-    engine : sqlalchemy.Engine
-        Engine that is already connected to a database
-    transactions_t : sqlalchemy.Table, optional
-        Table object for the transactions table. If not provided, the default orca.transactions table is used
-    '''
-    def __init__(self, start_date: datetime, end_date: datetime, engine: Engine, transactions_t: Table | None = None):
-        self.start_date = start_date
-        self.end_date = end_date
-        self.engine = engine
-        self.transactions_t = transactions_t
-        self.get_automap_bases()
-
-    def get_automap_bases(self):
-        self.Base_dssg = get_automap_base_with_views(engine=self.engine, schema=DSSG_SCHEMA)
-        self.Base_trac = get_automap_base_with_views(engine=self.engine, schema=TRAC_SCHEMA)
-        self.Base_orca = get_automap_base_with_views(engine=self.engine, schema=ORCA_SCHEMA)
-        self.Base_gtfs = get_automap_base_with_views(engine=self.engine, schema=GTFS_SCHEMA)
-    
-    def get_latest_gtfs_feed(self) -> Select:
-        '''
-        This function returns a query that can be used to get the latest GTFS feed for each transit agency.
-
-        Returns
-        -------
-        select : sqlalchemy.sql.selectable.Select
-            A select query that can be used to get the latest GTFS feed for each transit agency
-        '''
-        feed_info = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.FEED_INFO_TABLE.value]
-
-        # Get the feeds only for the given date range
-        stmt_gtfs_feed = \
-            select(feed_info)\
-            .where(not_(or_(feed_info.c.feed_start_date >= self.start_date, 
-                            feed_info.c.feed_end_date <= self.end_date)))
-
-        stmt_gtfs_feed_alias = stmt_gtfs_feed.subquery('feed')
-
-        # Rank the feeds for the given date range
-        stmt_gtfs_feed_ranked = \
-        select(stmt_gtfs_feed_alias, func.row_number().over(
-            partition_by=stmt_gtfs_feed_alias.c.feed_publisher_name,
-            order_by=stmt_gtfs_feed_alias.c.feed_id.desc()).label('feed_rank'))
-
-        stmt_gtfs_feed_ranked_alias = stmt_gtfs_feed_ranked.subquery('feed_ranked')
-
-        # Finally, get latest feed within the date range, for each transit agency, to get the feed_latest CTE
-        stmt_gtfs_feed_latest = \
-            select(stmt_gtfs_feed_ranked_alias)\
-            .where(stmt_gtfs_feed_ranked_alias.c.feed_rank == 1)
-
-        return stmt_gtfs_feed_latest
-    
-    def get_stop_details_from_feed(self, stmt_gtfs_feed: Select) -> Select:
-        '''
-        This function returns a query that can be used to get the stop details based on the given feeds.
-        Thus, contains gtfs agency details as well (including gtfs agency id).
-
-        Parameters
-        ----------
-        stmt_gtfs_feed : sqlalchemy.sql.selectable.Select
-            A select query that can be used to get relevant GTFS feeds based on customizable logic
-
-        Returns
-        -------
-        select : sqlalchemy.sql.selectable.Select
-            A select query that can be used to get the stop details based on the given feeds
-        '''
-        stops = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.STOPS_TABLE.value]
-        agencies_gtfs = self.Base_gtfs.metadata.tables[GTFS_SCHEMA_TABLES.AGENCY_TABLE.value]
-
-        stmt_gtfs_feed_alias = stmt_gtfs_feed.cte('feed_custom')
-
-        stmt_stop_custom = \
-            select(stops, agencies_gtfs.c.agency_id, agencies_gtfs.c.agency_name)\
-            .join(stmt_gtfs_feed_alias, stops.c.feed_id == stmt_gtfs_feed_alias.c.feed_id)\
-            .join(agencies_gtfs, stmt_gtfs_feed_alias.c.feed_id == agencies_gtfs.c.feed_id)
-
-        return stmt_stop_custom
-    
-    def get_transactions_with_agency(self) -> Select:
-        '''
-        This function returns a query that can be used to get transactions 
-
-        Returns
-        -------
-        select : sqlalchemy.sql.selectable.Select
-            A select query that can be used to get transactions with their agency details
-        '''
-        agencies = self.Base_trac.metadata.tables[TRAC_SCHEMA_TABLES.AGENCIES_TABLE.value]
-        stmt_transactions_with_agency = \
-            select(self.transactions_t, agencies.c.agency_id, agencies.c.orca_agency_id, agencies.c.gtfs_agency_id, agencies.c.agency_name)\
-            .join(agencies, self.transactions_t.c.source_agency_id == agencies.c.orca_agency_id)
-        return stmt_transactions_with_agency
-
-    def get_transactions_with_stop_or_device_locations(self, stmt_stop_location: Select) -> Select:
-        '''
-        This function returns a query that can be used to get transactions with their stop or device locations.
-        The stop location is used as the transaction location if present in stmt_stop_location, else the device location is used.
-
-        Parameters
-        ----------
-        stmt_stop_location : sqlalchemy.sql.selectable.Select
-            A select query that can be used to get stop location details based on customizable logic
-            Based on current implementation, this should be (or compatible with) the output of get_stop_details_from_feed
-        '''
-        return None
-
 def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, end_date: datetime, 
     automap_base_dict: dict, transactions_t: Table | None = None) -> Select:
     '''
@@ -224,8 +107,7 @@ def get_stop_locations_from_transactions_and_latest_gtfs(start_date: datetime, e
     stmt_transactions_with_location = \
     select(stmt_transactions_with_agency_alias,
         case((stmt_stop_latest_alias.c.stop_location.is_not(None), stmt_stop_latest_alias.c.stop_location),
-            else_=stmt_transactions_with_agency_alias.c.device_location).label('transaction_location'),
-        stmt_stop_latest_alias)\
+            else_=stmt_transactions_with_agency_alias.c.device_location).label('transaction_location'))\
         .join(stmt_stop_latest_alias,
             and_(stmt_transactions_with_agency_alias.c.stop_code == stmt_stop_latest_alias.c.stop_id,
                 stmt_transactions_with_agency_alias.c.gtfs_agency_id == stmt_stop_latest_alias.c.agency_id),
