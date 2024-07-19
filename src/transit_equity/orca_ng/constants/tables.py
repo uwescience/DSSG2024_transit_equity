a8f1017cfcc18b37962dd3ae2d6e3caaaec5cea8
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-19 14:11:51 -0700

    Update constants file to have a more intuitive naming convention

--- src/transit_equity/orca_ng/constants/tables.py
+++ src/transit_equity/orca_ng/constants/tables.py
@@ -9,6 +9,9 @@ class DSSG_TABLES(Enum):
     '''
     Access using DSSG_TABLES.<table_name>.value
     '''
+    V_BOARDINGS_APR2023: str = 'v_boardings_apr2023'
+    M_TRANSACTIONS_APR2023: str = 'm_transactions_apr2023'
+
     BOARDINGS_VIEW: str = 'v_boardings_apr2023'
     TRANSACTIONS_MAT_VIEW: str = 'm_transactions_apr2023'
 
@@ -16,6 +19,10 @@ class ORCA_TABLES(Enum):
     '''
     Access using ORCA_TABLES.<table_name>.value
     '''
+    TRANSACTIONS: str = 'transactions'
+    TRANSACTION_TYPES: str = 'transaction_types'
+    MODES: str = 'modes'
+
     TRANSACTIONS_TABLE: str = 'transactions',
     TRANSACTION_TYPES_TABLE: str = 'transaction_types',
     MODES_TABLE: str = 'modes'
@@ -24,12 +31,20 @@ class TRAC_TABLES(Enum):
     '''
     Access using TRAC_TABLES.<table_name>.value
     '''
+    AGENCIES: str = 'agencies'
+
     AGENCIES_TABLE: str = 'agencies'
+
 
 class GTFS_TABLES(Enum):
     '''
     Access using GTFS_TABLES.<table_name>.value
     '''
+    TL_FEED_INFO: str = 'tl_feed_info'
+    TRANSITLAND_FEEDS: str = 'transitland_feeds'
+    TL_STOPS: str = 'tl_stops'
+    TL_AGENCY: str = 'tl_agency'
+
     FEED_INFO_TABLE: str = 'tl_feed_info'
     FEEDS_TABLE: str = 'transitland_feeds'
     STOPS_TABLE: str = 'tl_stops'
