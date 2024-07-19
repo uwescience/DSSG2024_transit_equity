d83b2f5721ebab272a666ab93cc03f19baa3554f
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-19 10:18:26 -0700

    Remove linked_transactions constant from dssg

--- src/transit_equity/orca_ng/constants/schema_tables.py
+++ src/transit_equity/orca_ng/constants/schema_tables.py
@@ -13,7 +13,6 @@ class DSSG_SCHEMA_TABLES(Enum):
     Access using DSSG_SCHEMA_TABLES.<table_name>.value
     '''
     BOARDINGS_VIEW: str = f'{DSSG_SCHEMA}.v_boardings_apr2023'
-    LINKED_TRANSACTIONS_VIEW: str = f'{DSSG_SCHEMA}.v_linked_transactions_apr2023'
     TRANSACTIONS_MAT_VIEW: str = f'{DSSG_SCHEMA}.m_transactions_apr2023'
 
 class ORCA_SCHEMA_TABLES(Enum):
