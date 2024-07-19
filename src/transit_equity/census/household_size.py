a8e91ed634d75f034481b976b38aefb80a994fdd
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-19 13:57:04 -0700

    Remove redundant dictionaries

--- src/transit_equity/census/household_size.py
+++ src/transit_equity/census/household_size.py
@@ -69,25 +69,7 @@ class HOUSEHOLD_SIZE_COLUMNS(Enum):
     B11016_016E: HouseholdSizeDetails = HouseholdSizeDetails(field='B11016_016E',
         label='nonfamily_7_or_more_person_household', count=7)
 
-# The main motive of this dictionary is to map the column names to human readable names
-HOUSEHOLD_SIZE_COLUMNS_DICT = {
-    'B11016_001E': 'total',
-    'B11016_002E': 'family_households',
-    'B11016_003E': 'family_2_person_household',
-    'B11016_004E': 'family_3_person_household',
-    'B11016_005E': 'family_4_person_household',
-    'B11016_006E': 'family_5_person_household',
-    'B11016_007E': 'family_6_person_household',
-    'B11016_008E': 'family_7_or_more_person_household',
-    'B11016_009E': 'nonfamily_households',
-    'B11016_010E': 'nonfamily_1_person_household',
-    'B11016_011E': 'nonfamily_2_person_household',
-    'B11016_012E': 'nonfamily_3_person_household',
-    'B11016_013E': 'nonfamily_4_person_household',
-    'B11016_014E': 'nonfamily_5_person_household',
-    'B11016_015E': 'nonfamily_6_person_household',
-    'B11016_016E': 'nonfamily_7_or_more_person_household',
-}
+
 
 # TODO: This function can be extended to do a calculation on an entire pandas DataFrame
 # The extended function will also have better performance.
--- src/transit_equity/census/income.py
+++ src/transit_equity/census/income.py
@@ -77,26 +77,6 @@ class INCOME_DISTRIBUTION_COLUMNS(Enum):
     B19001_017E: IncomeDetails = IncomeDetails(field='B19001_017E',
         label='200000_or_more', min_income=200000, max_income=200000)
 
-# The main motive of this dictionary is to map the column names to human readable names
-INCOME_DISTRIBUTION_COLUMNS_DICT = {
-    'B19001_001E': 'total',
-    'B19001_002E': 'less_than_10000',
-    'B19001_003E': '10000_to_14999',
-    'B19001_004E': '15000_to_19999',
-    'B19001_005E': '20000_to_24999',
-    'B19001_006E': '25000_to_29999',
-    'B19001_007E': '30000_to_34999',
-    'B19001_008E': '35000_to_39999',
-    'B19001_009E': '40000_to_44999',
-    'B19001_010E': '45000_to_49999',
-    'B19001_011E': '50000_to_59999',
-    'B19001_012E': '60000_to_74999',
-    'B19001_013E': '75000_to_99999',
-    'B19001_014E': '100000_to_124999',
-    'B19001_015E': '125000_to_149999',
-    'B19001_016E': '150000_to_199999',
-    'B19001_017E': '200000_or_more',
-}
 
 # TODO: This function can be extended to do a calculation on an entire pandas DataFrame
 # The extended function will also have better performance.
