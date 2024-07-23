f3c311930b4e0e961b83ad126bb75d54f0cd5e42
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-23 16:14:28 -0700

    Add docstrings with examples to get_population_in_income_level_range

--- src/transit_equity/census/income_poverty_level.py
+++ src/transit_equity/census/income_poverty_level.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from enum import Enum
 
+LOW_INCOME_RANGE = (0, 2.0)
+
 class IncomePovertyLevelDetails:
     """
     A class to store the details of the poverty level columns in the census data
@@ -59,7 +61,8 @@ class INCOME_POVERTY_LEVEL_COLUMNS(Enum):
         label='2_to_2.99', min_income_level=2.0, max_income_level=1e6)
     
 def get_population_in_income_level_range(income_poverty_level_row: pd.Series, 
-                                         min_income_level: int, max_income_level: int):
+                                         min_income_level: int = LOW_INCOME_RANGE[0], 
+                                         max_income_level: int = LOW_INCOME_RANGE[1]):
     """
     A function to get the population in the given income to poverty level ratio range from a census row.
     Since the income to poverty level ratio is a continuous variable, 
@@ -84,6 +87,38 @@ def get_population_in_income_level_range(income_poverty_level_row: pd.Series,
     --------
     int
         The population in the given income to poverty level ratio range
+    
+    Examples:
+    ---------
+    Example 1:
+
+    The following example shows the calculation of population in a given income to poverty level ratio range from a census row
+    with 100 people in total, 20 people with income to poverty level ratio less than 0.5,
+    30 people with income to poverty level ratio between 0.5 and 0.99, 
+    10 people with income to poverty level ratio between 1.0 and 1.24, 
+    40 people with income to poverty level ratio between 2.0 and above, 
+    and 0 people with income to poverty level ratio in between.
+
+    The income to poverty level ratio range is [0, 2.0).
+    
+    Expected Answer: (20 + 30 + 10) = 60
+
+    >>> import pandas as pd
+    >>> from transit_equity.census.income_poverty_level import INCOME_POVERTY_LEVEL_COLUMNS
+    >>> from transit_equity.census.income_poverty_level import get_population_in_income_level_range
+    >>> income_poverty_level_dict = {
+    ...     'C17002_001E': 100,
+    ...     'C17002_002E': 20,
+    ...     'C17002_003E': 30,
+    ...     'C17002_004E': 10,
+    ...     'C17002_005E': 0,
+    ...     'C17002_006E': 0,
+    ...     'C17002_007E': 0,
+    ...     'C17002_008E': 40,
+    }
+    >>> income_poverty_level_row = pd.Series(income_poverty_level_dict)
+    >>> get_population_in_income_level_range(income_poverty_level_row, 0, 2.0)
+    60
     """
     population = 0
     for column in INCOME_POVERTY_LEVEL_COLUMNS:
