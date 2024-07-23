bd52aeca9783c1896ad11758afbaab6472cc7056
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-23 16:30:09 -0700

    Rename income_level to the desired income_poverty_level

--- src/transit_equity/census/income_poverty_level.py
+++ src/transit_equity/census/income_poverty_level.py
@@ -13,19 +13,19 @@ class IncomePovertyLevelDetails:
         The field name in the census data
     label: str
         The human readable label for the field
-    min_income_level: int
+    min_income_poverty_level: int
         The minimum income to poverty level ratio
-    max_income_level: int
+    max_income_poverty_level: int
         The maximum income to poverty level ratio. Exclusive.
-        Thus the range is [min_income_level, max_income_level)
+        Thus the range is [min_income_poverty_level, max_income_poverty_level)
     """
-    def __init__(self, field: str, label: str, min_income_level: int = 0, max_income_level: int = 0):
+    def __init__(self, field: str, label: str, min_income_poverty_level: int = 0, max_income_poverty_level: int = 0):
         # As it turns out, we do not need to use the @property decorator for the class attributes
         # since we are not using any setter methods
         self.field = field
         self.label = label
-        self.min_income_level = min_income_level
-        self.max_income_level = max_income_level
+        self.min_income_poverty_level = min_income_poverty_level
+        self.max_income_poverty_level = max_income_poverty_level
 
 class INCOME_POVERTY_LEVEL_COLUMNS(Enum):
     """
@@ -34,35 +34,35 @@ class INCOME_POVERTY_LEVEL_COLUMNS(Enum):
 
     Table: C17002 - RATIO OF INCOME TO POVERTY LEVEL IN THE PAST 12 MONTHS
 
-    The min_income_level and max_income_level have been assigned as -1 for the total field, 
+    The min_income_poverty_level and max_income_poverty_level have been assigned as -1 for the total field, 
         since they are not used in the calculation of average income to poverty level ratio
     
-    The max_income_level for the last column has been arbitrarily assigned as 1e6 
+    The max_income_poverty_level for the last column has been arbitrarily assigned as 1e6 
         since it is the highest income to poverty level ratio range.
         It is not recommended to use the last income to poverty level ratio range for any calculations.
     
     # TODO: Check an actual appropriate range for the last column
     """
     C17002_001E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_001E',
-        label='total', min_income_level=-1, max_income_level=-1)
+        label='total', min_income_poverty_level=-1, max_income_poverty_level=-1)
     C17002_002E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_002E',
-        label='less_than_0.5', min_income_level=0, max_income_level=0.5)
+        label='less_than_0.5', min_income_poverty_level=0, max_income_poverty_level=0.5)
     C17002_003E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_003E',
-        label='0.5_to_0.99', min_income_level=0.5, max_income_level=1.0)
+        label='0.5_to_0.99', min_income_poverty_level=0.5, max_income_poverty_level=1.0)
     C17002_004E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_004E',
-        label='1_to_1.24', min_income_level=1.0, max_income_level=1.25)
+        label='1_to_1.24', min_income_poverty_level=1.0, max_income_poverty_level=1.25)
     C17002_005E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_005E',
-        label='1.25_to_1.49', min_income_level=1.25, max_income_level=1.50)
+        label='1.25_to_1.49', min_income_poverty_level=1.25, max_income_poverty_level=1.50)
     C17002_006E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_006E',
-        label='1.5_to_1.84', min_income_level=1.50, max_income_level=1.85)
+        label='1.5_to_1.84', min_income_poverty_level=1.50, max_income_poverty_level=1.85)
     C17002_007E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_007E',
-        label='1.85_to_1.99', min_income_level=1.85, max_income_level=2.0)
+        label='1.85_to_1.99', min_income_poverty_level=1.85, max_income_poverty_level=2.0)
     C17002_008E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_008E',
-        label='2_to_above', min_income_level=2.0, max_income_level=1e6)
+        label='2_to_above', min_income_poverty_level=2.0, max_income_poverty_level=1e6)
     
-def get_population_in_income_level_range(income_poverty_level_row: pd.Series, 
-                                         min_income_level: int = LOW_INCOME_RANGE[0],
-                                         max_income_level: int = LOW_INCOME_RANGE[1]):
+def get_population_in_income_poverty_level_range(income_poverty_level_row: pd.Series, 
+                                         min_income_poverty_level: int = LOW_INCOME_RANGE[0],
+                                         max_income_poverty_level: int = LOW_INCOME_RANGE[1]):
     """
     A function to get the population in the given income to poverty level ratio range from a census row.
     Since the income to poverty level ratio is a continuous variable, 
@@ -76,19 +76,19 @@ def get_population_in_income_level_range(income_poverty_level_row: pd.Series,
         It contains different income to poverty level ratio ranges (e.g. less_than_0.5, 0.5_to_0.99, etc.)
             in a census area.
         
-    min_income_level: int
+    min_income_poverty_level: int
         The minimum income to poverty level ratio for the range
     
-    max_income_level: int
+    max_income_poverty_level: int
         The maximum income to poverty level ratio for the range. Exclusive.
-        Thus the range is [min_income_level, max_income_level)
+        Thus the range is [min_income_poverty_level, max_income_poverty_level)
     
     Returns:
     --------
     population
         The population in the given income to poverty level ratio range
     
-    true_income_level_range: Tuple[int, int]
+    true_income_poverty_level_range: Tuple[int, int]
         The actual income to poverty level ratio range that was used in the calculation
         This is useful when the input range is not within the bounds of the income to poverty level ratio ranges
         It will be the intersection of the input range and the income to poverty level ratio ranges
@@ -110,7 +110,7 @@ def get_population_in_income_level_range(income_poverty_level_row: pd.Series,
 
     >>> import pandas as pd
     >>> from transit_equity.census.income_poverty_level import INCOME_POVERTY_LEVEL_COLUMNS
-    >>> from transit_equity.census.income_poverty_level import get_population_in_income_level_range
+    >>> from transit_equity.census.income_poverty_level import get_population_in_income_poverty_level_range
     >>> income_poverty_level_dict = {
     ...     'C17002_001E': 100,
     ...     'C17002_002E': 20,
@@ -122,29 +122,29 @@ def get_population_in_income_level_range(income_poverty_level_row: pd.Series,
     ...     'C17002_008E': 40,
     }
     >>> income_poverty_level_row = pd.Series(income_poverty_level_dict)
-    >>> get_population_in_income_level_range(income_poverty_level_row, 0, 2.0)
+    >>> get_population_in_income_poverty_level_range(income_poverty_level_row, 0, 2.0)
     60
     """
     population = 0
-    true_min_income_level = min_income_level
-    true_max_income_level = max_income_level
+    true_min_income_poverty_level = min_income_poverty_level
+    true_max_income_poverty_level = max_income_poverty_level
     for column in INCOME_POVERTY_LEVEL_COLUMNS:
         if column.value.field not in income_poverty_level_row:
             continue
-        # Check if the value of the field is within [min_income_level, max_income_level]
-        if column.value.min_income_level >= min_income_level and column.value.max_income_level < max_income_level:
+        # Check if the value of the field is within [min_income_poverty_level, max_income_poverty_level]
+        if column.value.min_income_poverty_level >= min_income_poverty_level and column.value.max_income_poverty_level < max_income_poverty_level:
             population += income_poverty_level_row[column.value.field]
-        # Else check if the value of the field has no overlap with [min_income_level, max_income_level]
-        elif column.value.min_income_level >= max_income_level or column.value.max_income_level < min_income_level:
+        # Else check if the value of the field has no overlap with [min_income_poverty_level, max_income_poverty_level]
+        elif column.value.min_income_poverty_level >= max_income_poverty_level or column.value.max_income_poverty_level < min_income_poverty_level:
             pass
-        # Else there is some overlap with [min_income_level, max_income_level)
+        # Else there is some overlap with [min_income_poverty_level, max_income_poverty_level)
         else:
             population += income_poverty_level_row[column.value.field]
-            if column.value.min_income_level < min_income_level:
-                true_min_income_level = column.value.min_income_level
-            if column.value.max_income_level > max_income_level:
-                true_max_income_level = column.value.max_income_level
+            if column.value.min_income_poverty_level < min_income_poverty_level:
+                true_min_income_poverty_level = column.value.min_income_poverty_level
+            if column.value.max_income_poverty_level > max_income_poverty_level:
+                true_max_income_poverty_level = column.value.max_income_poverty_level
     return {
         'population': population,
-        'true_income_level_range': (true_min_income_level, true_max_income_level)
+        'true_income_poverty_level_range': (true_min_income_poverty_level, true_max_income_poverty_level)
     }
