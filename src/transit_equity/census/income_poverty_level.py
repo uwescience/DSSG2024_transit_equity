1a9de7f9ad1e399e0cd7b0b142e87b8d86961751
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-23 15:54:50 -0700

    Add columns and function setup to income_poverty_level

--- src/transit_equity/census/income_poverty_level.py
+++ src/transit_equity/census/income_poverty_level.py
@@ -14,7 +14,7 @@ class IncomePovertyLevelDetails:
     min_income_level: int
         The minimum income to poverty level ratio
     max_income_level: int
-        The maximum income to poverty level ratio
+        The maximum income to poverty level ratio. Exclusive.
     """
     def __init__(self, field: str, label: str, min_income_level: int = 0, max_income_level: int = 0):
         # As it turns out, we do not need to use the @property decorator for the class attributes
@@ -36,8 +36,27 @@ class INCOME_POVERTY_LEVEL_COLUMNS(Enum):
     
     The max_income_level for the last column has been arbitrarily assigned as 1e6 
         since it is the highest income to poverty level ratio range.
+        It is not recommended to use the last income to poverty level ratio range for any calculations.
+    
     # TODO: Check an actual appropriate range for the last column
     """
     C17002_001E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_001E',
         label='total', min_income_level=-1, max_income_level=-1)
+    C17002_002E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_002E',
+        label='less_than_0.5', min_income_level=0, max_income_level=0.5)
+    C17002_003E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_003E',
+        label='0.5_to_0.99', min_income_level=0.5, max_income_level=1.0)
+    C17002_004E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_004E',
+        label='1_to_1.24', min_income_level=1.0, max_income_level=1.25)
+    C17002_005E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_005E',
+        label='1.25_to_1.49', min_income_level=1.25, max_income_level=1.50)
+    C17002_006E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_006E',
+        label='1.5_to_1.84', min_income_level=1.50, max_income_level=1.85)
+    C17002_007E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_007E',
+        label='1.85_to_1.99', min_income_level=1.85, max_income_level=2.0)
+    C17002_008E: IncomePovertyLevelDetails = IncomePovertyLevelDetails(field='C17002_008E',
+        label='2_to_2.99', min_income_level=2.0, max_income_level=1e6)
     
+def get_population_in_income_level_range(income_poverty_level_row: pd.Series, 
+                                         min_income_level: int, max_income_level: int):
+    pass
