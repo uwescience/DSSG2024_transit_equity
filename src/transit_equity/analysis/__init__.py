00a05b596d5df658fa8f9d0bbb1eae5848c37554
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-24 15:49:07 -0700

    Add separate analysis folder for specific analyses

---
+++ src/transit_equity/analysis/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package contains module for helper functions for various kinds of analysis of transit equity data.
+
+The modules and the contained functions in this package are currently very specific to their analysis 
+    and are not intended to be used as a general-purpose library.
+
+Modules:
+    - 'low_income': Helper Functions for Analysis of low-income populations and transit access
+
+"""
---
+++ src/transit_equity/analysis/low_income/__init__.py
@@ -0,0 +1,3 @@
+"""
+This module contains the helper Functions to facilitate analysis of low-income populations and transit access.
+"""
