1735378eee7978ec651f16f3ae2ea3b9bc973892
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-24 15:01:31 -0700

    Removed method to process series due to redundancy with dataframe based method

---
+++ src/transit_equity/analysis/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package contains module for various kinds of analysis of transit equity data.
+
+The modules in this package are currently very specific to their analysis 
+    and are not intended to be used as a general-purpose library.
+
+Modules:
+    - 'low_income': Analysis of low-income populations and transit access
+
+"""
