9e67a082a79ce2f7896f57e4dc52a567158052a7
tatasky09 <ningsiman@hotmail.com>
2024-07-26 16:12:34 -0700

    changed tables

--- src/transit_equity/orca/constants/schema_tables.py
+++ src/transit_equity/orca/constants/schema_tables.py
@@ -12,44 +12,151 @@ class DSSG_SCHEMA_TABLES(Enum):
     """
     Access using DSSG_SCHEMA_TABLES.<table_name>.value
     """
-    V_BOARDINGS_APR2023: str = f'{DSSG_SCHEMA}.v_boardings_apr2023'
-    V_LINKED_TRANSACTIONS_APR2023: str = f'{DSSG_SCHEMA}.v_linked_transactions_apr2023'
-    M_TRANSACTIONS_APR2023: str = f'{DSSG_SCHEMA}.m_transactions_apr2023' 
 
     BOARDINGS_VIEW: str = f'{DSSG_SCHEMA}.v_boardings_apr2023'
     LINKED_TRANSACTIONS_VIEW: str = f'{DSSG_SCHEMA}.v_linked_transactions_apr2023'
     TRANSACTIONS_MAT_VIEW: str = f'{DSSG_SCHEMA}.m_transactions_apr2023'
 
+    #new
+    CT_BUS_STOPS: str = f'{DSSG_SCHEMA}.ct_bus_stops'
+    KCM_STOPS: str = f'{DSSG_SCHEMA}.kcm_stops'
+    KCM_EQ_JOB_EQUITY_ROUTES: str = f'{DSSG_SCHEMA}.kcm_eq_job_equity_routes'
+    KCM_EQ_EQUITY_PRIORITY_AREAS: str = f'{DSSG_SCHEMA}.kcm_eq_equity_priority_areas'
+    KCM_EQ_JOB_EQUITY_PRIORITY_AREAS: str = f'{DSSG_SCHEMA}.kcm_eq_job_equity_priority_areas'
+    V_BOARDINGS_APR2023: str = f'{DSSG_SCHEMA}.v_boardings_apr2023'
+    V_LINKED_TRANSACTIONS_APR2023: str = f'{DSSG_SCHEMA}.v_linked_transactions_apr2023'
+    V_TRANSACTIONS_APR2023: str = f'{DSSG_SCHEMA}.v_transactions_apr2023'
+    M_TXN_IDS_APR2023: str = f'{DSSG_SCHEMA}.m_txn_ids_apr2023'
+    M_TRANSACTIONS_APR2023: str = f'{DSSG_SCHEMA}.m_transactions_apr2023'
+
+
 class ORCA_SCHEMA_TABLES(Enum):
     """
     Access using ORCA_SCHEMA_TABLES.<table_name>.value
     """
-    TRANSACTIONS: str = f'{ORCA_SCHEMA}.transactions'
-    TRANSACTION_TYPES: str = f'{ORCA_SCHEMA}.transaction_types'
-    MODES: str = f'{ORCA_SCHEMA}.modes'
-
+ 
     TRANSACTIONS_TABLE: str = f'{ORCA_SCHEMA}.transactions',
     TRANSACTION_TYPES_TABLE: str = f'{ORCA_SCHEMA}.transaction_types',
     MODES_TABLE: str = f'{ORCA_SCHEMA}.modes'
+
+    #new
+    STOPS: str = f'{ORCA_SCHEMA}.stops'
+    FARE_PRODUCTS: str = f'{ORCA_SCHEMA}.fare_products'
+    LINKED_TRANSACTIONS: str = f'{ORCA_SCHEMA}.linked_transactions'
+    CARD_TRANSIT_ACCOUNTS: str = f'{ORCA_SCHEMA}.card_transit_accounts'
+    INSERT_DATETIMES: str = f'{ORCA_SCHEMA}.insert_datetimes'
+    FILES: str = f'{ORCA_SCHEMA}.files'
+    ORGANIZATION_NAMES: str = f'{ORCA_SCHEMA}.organization_names'
+    PARTICIPANT_GROUPS: str = f'{ORCA_SCHEMA}.participant_groups'
+    LINKED_TRANSACTIONS_0250M: str = f'{ORCA_SCHEMA}.linked_transactions_0250m'
+    TRANSACTION_TYPES: str = f'{ORCA_SCHEMA}.transaction_types'
+    TRANSACTIONS: str = f'{ORCA_SCHEMA}.transactions'
+    TRANSACTIONS_0250M: str = f'{ORCA_SCHEMA}.transactions_0250m'
+    LINKED_TRANSACTIONS_0500M: str = f'{ORCA_SCHEMA}.linked_transactions_0500m'
+    PASSENGER_TYPES: str = f'{ORCA_SCHEMA}.passenger_types'
+    DIRECTIONS: str = f'{ORCA_SCHEMA}.directions'
+    TRANSACTIONS_0500M: str = f'{ORCA_SCHEMA}.transactions_0500m'
+    ORGANIZATIONS: str = f'{ORCA_SCHEMA}.organizations'
+    TRANSACTIONS_0750M: str = f'{ORCA_SCHEMA}.transactions_0750m'
+    TRANSACTIONS_1000M: str = f'{ORCA_SCHEMA}.transactions_1000m'
+    LINKED_TRANSACTIONS_0750M: str = f'{ORCA_SCHEMA}.linked_transactions_0750m'
+    LINKED_TRANSACTIONS_1000M: str = f'{ORCA_SCHEMA}.linked_transactions_1000m'
+    CARD_CENSUS_BLOCK_GROUPS: str = f'{ORCA_SCHEMA}.card_census_block_groups'
+    CARDS: str = f'{ORCA_SCHEMA}.cards'
+    V_TRANSACTIONS: str = f'{ORCA_SCHEMA}.v_transactions'
+    V_BOARDINGS: str = f'{ORCA_SCHEMA}.v_boardings'
+    V_ALIGHTINGS: str = f'{ORCA_SCHEMA}.v_alightings'
+    M_TXN_TIMERANGES: str = f'{ORCA_SCHEMA}.m_txn_timeranges'
+    M_CARD_LATEST_TXN_DTM: str = f'{ORCA_SCHEMA}.m_card_latest_txn_dtm'
+    M_LATEST_FILE_UPLOAD: str = f'{ORCA_SCHEMA}.m_latest_file_upload'
+
 
 class TRAC_SCHEMA_TABLES(Enum):
     """
     Access using TRAC_SCHEMA_TABLES.<table_name>.value
     """
-    AGENCIES: str = f'{TRAC_SCHEMA}.agencies'
-
+   
     AGENCIES_TABLE: str = f'{TRAC_SCHEMA}.agencies'
+
+    #new
+    ST_BUS_OPERATORS: str = f'{TRAC_SCHEMA}.st_bus_operators'
+    ROUTE_NAME_LOOKUP: str = f'{TRAC_SCHEMA}.route_name_lookup'
+    PARAMETERS: str = f'{TRAC_SCHEMA}.parameters'
+    TRANSITLAND_AGENCIES: str = f'{TRAC_SCHEMA}.transitland_agencies'
+    MODES: str = f'{TRAC_SCHEMA}.modes'
+    AGENCIES: str = f'{TRAC_SCHEMA}.agencies'
+    BAD_TRANSACTIONS_0250M: str = f'{TRAC_SCHEMA}.bad_transactions_0250m'
+    BAD_TRANSACTIONS_0750M: str = f'{TRAC_SCHEMA}.bad_transactions_0750m'
+    BAD_TRANSACTIONS_0500M: str = f'{TRAC_SCHEMA}.bad_transactions_0500m'
+    BAD_TRANSACTIONS_1000M: str = f'{TRAC_SCHEMA}.bad_transactions_1000m'
+    BAD_TRANSACTIONS: str = f'{TRAC_SCHEMA}.bad_transactions'
+    E_GEOCODE_RESULT_TYPES: str = f'{TRAC_SCHEMA}.e_geocode_result_types'
+    E_BAD_TRANSACTION_TYPES: str = f'{TRAC_SCHEMA}.e_bad_transaction_types'
+    V_PREVIOUS_DUPLICATE_TXN: str = f'{TRAC_SCHEMA}.v_previous_duplicate_txn'
+
 
 class GTFS_SCHEMA_TABLES(Enum):
     """
     Access using GTFS_SCHEMA_TABLES.<table_name>.value
     """
-    TL_FEED_INFO: str = f'{GTFS_SCHEMA}.tl_feed_info'
-    TRANSITLAND_FEEDS: str = f'{GTFS_SCHEMA}.transitland_feeds'
-    TL_STOPS: str = f'{GTFS_SCHEMA}.tl_stops'
-    TL_AGENCY: str = f'{GTFS_SCHEMA}.tl_agency'
-
     FEED_INFO_TABLE: str = f'{GTFS_SCHEMA}.tl_feed_info'
     FEEDS_TABLE: str = f'{GTFS_SCHEMA}.transitland_feeds'
     STOPS_TABLE: str = f'{GTFS_SCHEMA}.tl_stops'
     AGENCY_TABLE: str = f'{GTFS_SCHEMA}.tl_agency'
+
+    ##new
+    TRANSITLAND_FEEDS: str = f'{GTFS_SCHEMA}.transitland_feeds'
+    TL_TRANSLATIONS: str = f'{GTFS_SCHEMA}.tl_translations'
+    TL_NETWORKS: str = f'{GTFS_SCHEMA}.tl_networks'
+    TL_FARE_MEDIA: str = f'{GTFS_SCHEMA}.tl_fare_media'
+    TL_FARE_RULES: str = f'{GTFS_SCHEMA}.tl_fare_rules'
+    E_LOCATION_TYPES: str = f'{GTFS_SCHEMA}.e_location_types'
+    TL_TIMEFRAMES: str = f'{GTFS_SCHEMA}.tl_timeframes'
+    TL_FARE_PRODUCTS: str = f'{GTFS_SCHEMA}.tl_fare_products'
+    E_EXCEPTION_TYPES: str = f'{GTFS_SCHEMA}.e_exception_types'
+    E_PAYMENT_METHODS: str = f'{GTFS_SCHEMA}.e_payment_methods'
+    E_TRANSFERS: str = f'{GTFS_SCHEMA}.e_transfers'
+    E_WHEELCHAIR_BOARDINGS: str = f'{GTFS_SCHEMA}.e_wheelchair_boardings'
+    E_DIRECTION_IDS: str = f'{GTFS_SCHEMA}.e_direction_ids'
+    E_WHEELCHAIR_ACCESSIBILITY: str = f'{GTFS_SCHEMA}.e_wheelchair_accessibility'
+    E_SERVICE_AVAILABILITY: str = f'{GTFS_SCHEMA}.e_service_availability'
+    E_ROUTE_TYPES: str = f'{GTFS_SCHEMA}.e_route_types'
+    TL_TRIPS: str = f'{GTFS_SCHEMA}.tl_trips'
+    E_BIKES_ALLOWED: str = f'{GTFS_SCHEMA}.e_bikes_allowed'
+    E_PICKUP_TYPES: str = f'{GTFS_SCHEMA}.e_pickup_types'
+    E_EXACT_TIMES: str = f'{GTFS_SCHEMA}.e_exact_times'
+    E_DURATION_LIMIT_TYPES: str = f'{GTFS_SCHEMA}.e_duration_limit_types'
+    E_TIMEPOINTS: str = f'{GTFS_SCHEMA}.e_timepoints'
+    E_TRANSFER_TYPES: str = f'{GTFS_SCHEMA}.e_transfer_types'
+    E_FARE_MEDIA_TYPES: str = f'{GTFS_SCHEMA}.e_fare_media_types'
+    E_IS_ROLE: str = f'{GTFS_SCHEMA}.e_is_role'
+    TL_EXTRA_FILES: str = f'{GTFS_SCHEMA}.tl_extra_files'
+    E_IS_BIDIRECTIONAL: str = f'{GTFS_SCHEMA}.e_is_bidirectional'
+    TL_AGENCY: str = f'{GTFS_SCHEMA}.tl_agency'
+    TL_FEED_INFO: str = f'{GTFS_SCHEMA}.tl_feed_info'
+    E_CONTINUOUS_PICKUP: str = f'{GTFS_SCHEMA}.e_continuous_pickup'
+    E_CONTINUOUS_DROP_OFF: str = f'{GTFS_SCHEMA}.e_continuous_drop_off'
+    TL_CALENDAR: str = f'{GTFS_SCHEMA}.tl_calendar'
+    TL_AREAS: str = f'{GTFS_SCHEMA}.tl_areas'
+    TL_ROUTES: str = f'{GTFS_SCHEMA}.tl_routes'
+    TL_CALENDAR_DATES: str = f'{GTFS_SCHEMA}.tl_calendar_dates'
+    E_DROP_OFF_TYPES: str = f'{GTFS_SCHEMA}.e_drop_off_types'
+    E_FARE_TRANSFER_TYPES: str = f'{GTFS_SCHEMA}.e_fare_transfer_types'
+    TL_FARE_ATTRIBUTES: str = f'{GTFS_SCHEMA}.tl_fare_attributes'
+    E_PATHWAY_MODES: str = f'{GTFS_SCHEMA}.e_pathway_modes'
+    TL_FARE_LEG_RULES: str = f'{GTFS_SCHEMA}.tl_fare_leg_rules'
+    TL_FARE_TRANSFER_RULES: str = f'{GTFS_SCHEMA}.tl_fare_transfer_rules'
+    TL_STOP_AREAS: str = f'{GTFS_SCHEMA}.tl_stop_areas'
+    TL_ROUTE_NETWORKS: str = f'{GTFS_SCHEMA}.tl_route_networks'
+    TL_FREQUENCIES: str = f'{GTFS_SCHEMA}.tl_frequencies'
+    TL_TRANSFERS: str = f'{GTFS_SCHEMA}.tl_transfers'
+    TL_ATTRIBUTIONS: str = f'{GTFS_SCHEMA}.tl_attributions'
+    TL_PATHWAYS: str = f'{GTFS_SCHEMA}.tl_pathways'
+    TL_SHAPES: str = f'{GTFS_SCHEMA}.tl_shapes'
+    TL_LEVELS: str = f'{GTFS_SCHEMA}.tl_levels'
+    TL_STOP_TIMES: str = f'{GTFS_SCHEMA}.tl_stop_times'
+    TL_STOPS: str = f'{GTFS_SCHEMA}.tl_stops'
+    TL_BAD_FEEDS: str = f'{GTFS_SCHEMA}.tl_bad_feeds'
+    V_TRANSITLAND_FEED_INFO: str = f'{GTFS_SCHEMA}.v_transitland_feed_info'
+
+
--- src/transit_equity/orca_ng/constants/tables.py
+++ src/transit_equity/orca_ng/constants/tables.py
@@ -9,43 +9,142 @@ class DSSG_TABLES(Enum):
     """
     Access using DSSG_TABLES.<table_name>.value
     """
-    V_BOARDINGS_APR2023: str = 'v_boardings_apr2023'
-    M_TRANSACTIONS_APR2023: str = 'm_transactions_apr2023'
 
     BOARDINGS_VIEW: str = 'v_boardings_apr2023'
     TRANSACTIONS_MAT_VIEW: str = 'm_transactions_apr2023'
+
+    ##new
+    V_BOARDINGS_APR2023: str ='v_boardings_apr2023'
+    M_TXN_IDS_APR2023: str ='m_txn_ids_apr2023'
+    M_TRANSACTIONS_APR2023: str ='m_transactions_apr2023'
+
+
 
 class ORCA_TABLES(Enum):
     """
     Access using ORCA_TABLES.<table_name>.value
     """
-    TRANSACTIONS: str = 'transactions'
-    TRANSACTION_TYPES: str = 'transaction_types'
-    MODES: str = 'modes'
 
     TRANSACTIONS_TABLE: str = 'transactions',
     TRANSACTION_TYPES_TABLE: str = 'transaction_types',
     MODES_TABLE: str = 'modes'
 
+    #new
+    DATA_UPLOADS: str ='data_uploads'
+    TRANSACTIONS: str ='transactions'
+    TRANSACTION_TYPES: str ='transaction_types'
+    LINKED_TRANSACTIONS: str ='linked_transactions'
+    MODES: str ='modes'
+    H_TRANSACTIONS: str ='h_transactions'
+    CARDS: str ='cards'
+    PASSENGER_TYPES: str ='passenger_types'
+    ORGANIZATIONS: str ='organizations'
+    DIRECTIONS: str ='directions'
+    PARTICIPANT_GROUPS: str ='participant_groups'
+    STOPS: str ='stops'
+    PRODUCTS: str ='products'
+    TRANSACTIONS_300M: str ='transactions_300m'
+    TRANSACTIONS_400M: str ='transactions_400m'
+    TRANSACTIONS_100M: str ='transactions_100m'
+    TRANSACTIONS_200M: str ='transactions_200m'
+    LINKED_TRANSACTIONS_100M: str ='linked_transactions_100m'
+    LINKED_TRANSACTIONS_200M: str ='linked_transactions_200m'
+    LINKED_TRANSACTIONS_300M: str ='linked_transactions_300m'
+    LINKED_TRANSACTIONS_400M: str ='linked_transactions_400m'
+    V_TRANSACTIONS: str ='v_transactions'
+    V_ALIGHTINGS: str ='v_alightings'
+    V_BOARDINGS: str ='v_boardings'
+    V_LINKED_TRANSACTIONS: str ='v_linked_transactions'
+    V_FINANCIAL_XFER_INFO: str ='v_financial_xfer_info'
+    V_ORCA_TRANSFER: str ='v_orca_transfer'
+    M_CARD_LATEST_TXN_DTM: str ='m_card_latest_txn_dtm'
+    M_TRANSACTION_DATES: str ='m_transaction_dates'
+
+
 class TRAC_TABLES(Enum):
     """
     Access using TRAC_TABLES.<table_name>.value
     """
-    AGENCIES: str = 'agencies'
 
     AGENCIES_TABLE: str = 'agencies'
+    #new
+    BAD_TRANSACTIONS: str ='bad_transactions'
+    AGENCIES: str ='agencies'
+    BAD_TRANSACTION_TYPES: str ='bad_transaction_types'
+    BAD_TRANSACTIONS_200M: str ='bad_transactions_200m'
+    DIRECTION_UPDATE_NOTE: str ='direction_update_note'
+    ROUTE_NAME_LOOKUP: str ='route_name_lookup'
+    STOP_UPDATE_NOTE: str ='stop_update_note'
+    BAD_TRANSACTIONS_100M: str ='bad_transactions_100m'
+    BAD_TRANSACTIONS_300M: str ='bad_transactions_300m'
+    BAD_TRANSACTIONS_400M: str ='bad_transactions_400m'
+    V_PREVIOUS_DUPLICATE_TXN: str ='v_previous_duplicate_txn'
+
 
 
 class GTFS_TABLES(Enum):
     """
     Access using GTFS_TABLES.<table_name>.value
     """
-    TL_FEED_INFO: str = 'tl_feed_info'
-    TRANSITLAND_FEEDS: str = 'transitland_feeds'
-    TL_STOPS: str = 'tl_stops'
-    TL_AGENCY: str = 'tl_agency'
 
     FEED_INFO_TABLE: str = 'tl_feed_info'
     FEEDS_TABLE: str = 'transitland_feeds'
     STOPS_TABLE: str = 'tl_stops'
     AGENCY_TABLE: str = 'tl_agency'
+
+    # new
+    E_CONTINUOUS_DROP_OFF: str ='e_continuous_drop_off'
+    E_CONTINUOUS_PICKUP: str ='e_continuous_pickup'
+    E_DROP_OFF_TYPES: str ='e_drop_off_types'
+    E_BIKES_ALLOWED: str ='e_bikes_allowed'
+    E_DIRECTION_IDS: str ='e_direction_ids'
+    E_FARE_TRANSFER_TYPES: str ='e_fare_transfer_types'
+    E_IS_BIDIRECTIONAL: str ='e_is_bidirectional'
+    E_SERVICE_AVAILABILITY: str ='e_service_availability'
+    E_DURATION_LIMIT_TYPES: str ='e_duration_limit_types'
+    E_EXACT_TIMES: str ='e_exact_times'
+    E_PATHWAY_MODES: str ='e_pathway_modes'
+    E_EXCEPTION_TYPES: str ='e_exception_types'
+    E_TIMEPOINTS: str ='e_timepoints'
+    E_ROUTE_TYPES: str ='e_route_types'
+    E_FARE_MEDIA_TYPES: str ='e_fare_media_types'
+    E_PICKUP_TYPES: str ='e_pickup_types'
+    E_PAYMENT_METHODS: str ='e_payment_methods'
+    E_IS_ROLE: str ='e_is_role'
+    E_LOCATION_TYPES: str ='e_location_types'
+    E_WHEELCHAIR_ACCESSIBILITY: str ='e_wheelchair_accessibility'
+    TL_FARE_MEDIA: str ='tl_fare_media'
+    E_TRANSFER_TYPES: str ='e_transfer_types'
+    TL_AGENCY: str ='tl_agency'
+    TL_AREAS: str ='tl_areas'
+    TL_CALENDAR: str ='tl_calendar'
+    TL_CALENDAR_DATES: str ='tl_calendar_dates'
+    TL_BAD_FEEDS: str ='tl_bad_feeds'
+    TL_FARE_ATTRIBUTES: str ='tl_fare_attributes'
+    TL_EXTRA_FILES: str ='tl_extra_files'
+    E_TRANSFERS: str ='e_transfers'
+    TL_FARE_LEG_RULES: str ='tl_fare_leg_rules'
+    TL_ATTRIBUTIONS: str ='tl_attributions'
+    E_WHEELCHAIR_BOARDINGS: str ='e_wheelchair_boardings'
+    TL_FARE_PRODUCTS: str ='tl_fare_products'
+    TL_FARE_RULES: str ='tl_fare_rules'
+    TL_FREQUENCIES: str ='tl_frequencies'
+    TL_FEED_INFO: str ='tl_feed_info'
+    TL_PATHWAYS: str ='tl_pathways'
+    TL_LEVELS: str ='tl_levels'
+    TL_NETWORKS: str ='tl_networks'
+    TL_ROUTES: str ='tl_routes'
+    TL_SHAPES: str ='tl_shapes'
+    TL_ROUTE_NETWORKS: str ='tl_route_networks'
+    TL_STOP_TIMES: str ='tl_stop_times'
+    TL_STOP_AREAS: str ='tl_stop_areas'
+    TL_STOPS: str ='tl_stops'
+    TL_FARE_TRANSFER_RULES: str ='tl_fare_transfer_rules'
+    TL_TIMEFRAMES: str ='tl_timeframes'
+    TL_TRIPS: str ='tl_trips'
+    TL_TRANSLATIONS: str ='tl_translations'
+    TRANSITLAND_AGENCIES: str ='transitland_agencies'
+    TL_TRANSFERS: str ='tl_transfers'
+    TRANSITLAND_FEEDS: str ='transitland_feeds'
+    V_TRANSITLAND_FEED_INFO: str ='v_transitland_feed_info'
+
