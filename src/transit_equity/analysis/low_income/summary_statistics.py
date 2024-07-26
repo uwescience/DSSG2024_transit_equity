c48e4b5864cf68e9c5ceee5c264f9d3c901f806f
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-26 10:57:26 -0700

    Add summary statistics code

---
+++ src/transit_equity/analysis/low_income/summary_statistics.py
@@ -0,0 +1,42 @@
+"""
+
+"""
+import pandas as pd
+import geopandas as gpd
+
+def summarize_census_block_counts_df(df_transactions_with_location: pd.DataFrame, 
+                                     gdf_block_group_counts: gpd.GeoDataFrame,
+                                     summary_column: str = 'txn_count',
+                                     percentiles: list = [0.25, 0.5, 0.75, 0.9, 0.95, 0.99]) -> list:
+    """
+    Summarize the counts of transactions per census block group.
+
+    Parameters
+    ----------
+    df_transactions_with_location : pd.DataFrame
+        A DataFrame containing the transactions with their locations
+    
+    gdf_block_group_counts : gpd.GeoDataFrame
+        A GeoDataFrame containing the counts of any summary column per census block group
+    
+    summary_column : str
+        The name of the column in the GeoDataFrame that contains the summary counts
+    
+    percentiles : list
+        A list of percentiles to calculate for the summary column
+    
+    Returns
+    -------
+    list
+        A list of strings containing the summary statistics
+    """
+    gdf_pg_bg_counts_summary = [f'Total Transactions: {df_transactions_with_location.shape[0]},'+ 
+                            f'Total Blocks: {gdf_block_group_counts.shape[0]}']
+    gdf_pg_bg_counts_summary.append(f'Average Transactions per Block: {df_transactions_with_location.shape[0] / gdf_block_group_counts.shape[0]}')
+    gdf_pg_bg_counts_summary.append(f'Min Counts per Block: {gdf_block_group_counts[summary_column].min()}, '+
+                                    f'Max Counts per Block: {gdf_block_group_counts[summary_column].max()}')
+    gdf_pg_bg_counts_summary.append(f'Median Counts per Block: {gdf_block_group_counts[summary_column].median()}')
+    gdf_pg_bg_counts_summary.append(f'\nPercentile Counts per Block:\n{gdf_block_group_counts[summary_column]\
+                                        .quantile(percentiles)}\n')
+    
+    return gdf_pg_bg_counts_summary
