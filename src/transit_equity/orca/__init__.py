fae5e606601bf7e5a1a36f956e064b8c590312c4
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-26 21:10:58 +0000

    TLC Week 7: Docstring updates on orca and orca_ng modules (#48)
    * Add additional docstrings to orca module
    
    * Add additional docstrings to orca_ng module
    
    * rename module to file lol
    
    * Add more context to the transactions_with_locations file

--- src/transit_equity/orca/__init__.py
+++ src/transit_equity/orca/__init__.py
@@ -1,3 +1,10 @@
 """
-This package stores common operations done on the orca database
+This package stores helper functions and variables for common operations done on the orca database
+
+Modules:
+-------
+constants: Contains constants used in the orca package
+    The constants include the schema names, and the respective table names.
+
+query: Contains functions to run various kinds of queries the orca database
 """
