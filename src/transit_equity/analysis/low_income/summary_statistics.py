d966805b8a8ec6e51f965ff54d833e43024a304f
Himanshu Naidu <hnaidu36@uw.edu>
2024-07-30 14:02:52 -0700

    Modified the summary function to only summarize the block group based counts

--- src/transit_equity/analysis/low_income/summary_statistics.py
+++ src/transit_equity/analysis/low_income/summary_statistics.py
@@ -6,27 +6,19 @@ Functions:
 summarize_census_block_counts_df:
     Summarize different counts and statistics of transactions per census block group.
 """
-import pandas as pd
 import geopandas as gpd
 
-def get_summary_census_block_counts_df(df_transactions_with_location: pd.DataFrame, 
-                                     gdf_block_group_counts: gpd.GeoDataFrame,
+def get_summary_census_block_counts_df(gdf_block_group_counts: gpd.GeoDataFrame,
                                      summary_column: str = 'txn_count',
                                      percentiles: list = None,
                                      transaction_label: str = 'Transaction') -> list:
     """
     Summarize the counts of transactions per census block group.
 
-    Warning: This function only makes sense if the gdf_block_group_counts is not filtered.
-
     Parameters
     ----------
-    df_transactions_with_location : pd.DataFrame
-        A DataFrame containing the transactions with their locations
-    
     gdf_block_group_counts : gpd.GeoDataFrame
         A GeoDataFrame containing the counts of any summary column per census block group
-        Warning: This function only makes sense if the gdf_block_group_counts is not filtered.
     
     summary_column : str
         The name of the column in the GeoDataFrame that contains the summary counts
@@ -42,9 +34,8 @@ def get_summary_census_block_counts_df(df_transactions_with_location: pd.DataFra
     if not percentiles:
         percentiles = [0.25, 0.5, 0.75, 0.9, 0.95, 0.99]
 
-    summary = [f'Total {transaction_label}s: {df_transactions_with_location.shape[0]},'+ 
-                            f'Total Blocks: {gdf_block_group_counts.shape[0]}']
-    summary.append(f'Average {transaction_label}s per Block: {df_transactions_with_location.shape[0] / gdf_block_group_counts.shape[0]}')
+    summary = [f'Total Blocks: {gdf_block_group_counts.shape[0]}']
+    summary.append(f'Average {transaction_label}s per Block: {gdf_block_group_counts[summary_column].mean()}')
     summary.append(f'Min {transaction_label}s per Block: {gdf_block_group_counts[summary_column].min()}, '+
                                     f'Max {transaction_label}s per Block: {gdf_block_group_counts[summary_column].max()}')
     summary.append(f'Median {transaction_label}s per Block: {gdf_block_group_counts[summary_column].median()}')
